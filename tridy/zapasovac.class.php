206f386e9a3f115f05203c4c786b2fa2654d4b3a
mrazekthom <mrazekthom@gmail.com>
2014-01-12 20:09:21 +0100

    Přidáno získávání ověřovacího formuláře

--- tridy/zapasovac.class.php
+++ tridy/zapasovac.class.php
@@ -1,35 +1,65 @@
 <?php
     class zapasovac {
         
-        private static $zapasy;
+    private $zapasy;
         
-        public function nacteniZapasu($pocet){
-            require('dbWrapper.class.php');
-            dbWrapper::pripoj();
-            $sql = dbWrapper::dotaz(<<<SQL
-                    SELECT `ID_domaci`, `ID_hoste`
-                    FROM `2014_zapasy_mladsi`
-                    WHERE `odehrano` = 0
-                    ORDER BY `cas_odehrani`
-                    LIMIT $pocet
+    public function nacteniZapasu($pocet){
+        require('dbWrapper.class.php'); #Může se smazat
+        dbWrapper::pripoj();
+        $sql = dbWrapper::dotaz(<<<SQL
+                SELECT `ID_domaci`, `ID_hoste`
+                FROM `2014_zapasy_mladsi`
+                WHERE `odehrano` = 0
+                ORDER BY `cas_odehrani`
+                LIMIT $pocet
 SQL
         ,Array());
-            self::$zapasy = $sql->FetchAll();
-        }
+        $this -> zapasy = $sql->FetchAll();
+    }
         
         
-        public function tym($zapas, $tym){
-            $parametr = self::$zapasy[$zapas][$tym];
-            $sql = dbWrapper::dotaz(<<<SQL
-                    SELECT jmeno
-                    FROM `2014_tymy_mladsi`
-                    WHERE id_teamu = ?
+    public function tym($zapas, $tym){
+        $parametr = $this -> zapasy[$zapas][$tym];
+        $sql = dbWrapper::dotaz(<<<SQL
+                SELECT jmeno
+                FROM `2014_tymy_mladsi`
+                WHERE id_teamu = ?
 SQL
         ,Array($parametr));
-            $vysledek = $sql->FetchAll();
-            $team = $vysledek[0]['jmeno'];
-            return $team;
+        $vysledek = $sql->FetchAll();
+        $team = $vysledek[0]['jmeno'];
+        return $team;
+    }
+
+
+    public function zjiskejOverovaciFormular($pole){
+        $formular = <<<FORMULAR
+            <table>
+FORMULAR;
+        for ($zapas=0; $zapas < 4; $zapas++) {
+            echo $zapas;
+            if (preg_match('/^[0-9]+$/', $_POST['score'][$zapas][0])){
+                if (preg_match('/^[0-9]+$/', $_POST['score'][$zapas][1])){
+                    $domaci = $this->tym($zapas,0);
+                    $hoste = $this->tym($zapas,1);
+                    $skoreDomaci = $_POST['score'][$zapas][0];
+                    $skoreHoste = $_POST['score'][$zapas][1];
+                    $formular .= <<<FORMULAR
+                        <tr>
+                            <td><p>$domaci</p></td>
+                            <td><p>$hoste</p></td>
+                        </tr>        
+                        <tr>
+                            <td><p>$skoreDomaci</p></td>
+                            <td><p>$skoreHoste</p></td>
+                        </tr>
+FORMULAR;
+                }
+            }
         }
-            
-    
+        $formular .= <<<FORMULAR
+            </table>
+FORMULAR;
+        return $formular;
+    }
 }
