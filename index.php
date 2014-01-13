7f8ce35c26e388f80b4ad928efc7f8b13b736977
Joe Kolář <thejoeejoee@gmail.com>
2014-01-13 22:35:41 +0100

    Přidán test na výstup zápasů

--- index.php
+++ index.php
@@ -4,21 +4,24 @@
         require("tridy/$trida.class.php");
 }
 spl_autoload_register("__autoload");
-
 dbWrapper::pripoj();
 
 
 
-$novinkovac = new novinkovac();
+$novinkovac = new Novinkovac();
 $content = "";
 if (isset($_POST['titulek'],$_POST['aktualita'])) {
 	$novinkovac->vlozNovinku($_POST['titulek'],$_POST['aktualita']);
 	header("Location: {$_SERVER['PHP_SELF']}");
 }
+$VystupZapasu = new VystupZapasu();
 
 $content .= $novinkovac->ziskejNovinky(5);
 $content .= "<hr><h2>Přidání novinky</h2>";
 $content .= $novinkovac->ziskejVkladaciFormular();
+$content .= "<hr><h2>Odehráné zápasy</h2>";
+$content .= $VystupZapasu->ziskejOdehraneZapasy("23-05-14");
+
 
 
 
@@ -34,7 +37,7 @@ $vystup = <<<SABLONA
 
     <script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>
     
-    <script type="text/javascript" src="js/jquery-te-1.4.0.js"></script>
+    <script type="text/javascript" src="js/jquery-te-1.4.0.min.js"></script>
 						
 
 </head>
