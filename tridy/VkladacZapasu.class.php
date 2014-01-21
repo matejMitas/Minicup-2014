01833bb3dfc8b38f769de46c3b80bf2ae163e9fa
mrazekthom <mrazekthom@gmail.com>
2014-01-21 20:28:11 +0100

    Přepsaná logika třídy + kontrola DB

--- tridy/VkladacZapasu.class.php
+++ tridy/VkladacZapasu.class.php
@@ -1,95 +1,94 @@
 <?php
-    class VkladacZapasu {
-        
-    private $zapasy;
-        
-    public function nacteniZapasu($pocet){
-        require('dbWrapper.class.php'); #Může se smazat
-        dbWrapper::pripoj();
-        $sql = dbWrapper::dotaz(<<<SQL
-                SELECT `ID_domaci`, `ID_hoste`
-                FROM `2014_zapasy_mladsi`
-                WHERE `odehrano` = 0
-                ORDER BY `cas_odehrani`
-                LIMIT $pocet
+	
+	class VkladacZapasu{
+
+		private $poleZapasu;
+
+
+		public function ziskejFormular($kategorie, $pocetZapasu, $post){
+			print_r($post);
+			echo "<br>";
+			if (isset($post['action'])){
+            	if ($post['action'] == "Zkontrolovat!"){
+            		$this -> ziskejZapasy($kategorie, $pocetZapasu);
+            		if ($this -> byloUzZapsano($post['id'])) {
+            			$return = $this -> overovaciFormular($post['score']);
+            		}else{
+            			$return = "<p>CHYBA</p>";
+            		}         		
+            	}elseif ($post['action'] == "Zapsat!") {
+                	
+            	}
+        	}else{
+            	$this -> ziskejZapasy($kategorie, $pocetZapasu);
+            	$return = $this -> vkladaciFormular();
+			}
+        	return $return;
+		}
+
+
+		public function ziskejZapasy($kategorie, $pocetZapasu){
+			require('dbWrapper.class.php'); #Může se smazat
+        	dbWrapper::pripoj(); #také smazat
+        	$zapasyKategorie = "2014_zapasy_" . $kategorie;
+        	$tymyKategorie = "2014_tymy_" . $kategorie;
+        	$sql = dbWrapper::dotaz(<<<SQL
+        		SELECT 
+        		b.`jmeno`,c.`jmeno`,`ID_zapasu` 
+				FROM $zapasyKategorie a
+  				JOIN $tymyKategorie b ON a.`ID_domaci` = b.`id_teamu`
+				JOIN $tymyKategorie c ON a.`ID_hoste` = c.`id_teamu`
+				WHERE a.`odehrano` = 0
+				ORDER BY a.`cas_odehrani`
+				LIMIT $pocetZapasu
 SQL
-        ,Array());
-        $this -> zapasy = $sql->FetchAll();
-    }
-        
-        
-    public function tym($zapas, $tym){
-        $parametr = $this -> zapasy[$zapas][$tym];
-        $sql = dbWrapper::dotaz(<<<SQL
-                SELECT jmeno
-                FROM `2014_tymy_mladsi`
-                WHERE id_teamu = ?
-SQL
-        ,Array($parametr));
-        $vysledek = $sql->FetchAll();
-        $team = $vysledek[0]['jmeno'];
-        return $team;
-    }
+			, Array());
+			$this -> pocetZapasu = $pocetZapasu;
+			$this -> poleZapasu = $sql->FetchAll(PDO::FETCH_NUM);
+			print_r($this -> poleZapasu);
+		}
 
 
-    public function ziskejFormular(){
-        $formular = <<<FORMULAR
-            <form action="test.php" method="post">
-                <table>
-FORMULAR;
-        for ($zapas=0; $zapas < 4; $zapas++) { 
-            $domaci = $this->tym($zapas,0);
-            $hoste = $this->tym($zapas,1);
-            $formular .= <<<FORMULAR
-                <tr>
-                    <td><p>$domaci</p></td>
-                    <td><p>$hoste</p></td>
-                </tr>
-                <tr>
-                    <td><input type="text" style="font-size: 30px;" size="2" name="score[$zapas][0]"></td>
-                    <td><input type="text" style="font-size: 30px;" size="2" name="score[$zapas][1]"></td>
-                </tr>
-FORMULAR;
-        }
-        $formular .= <<<FORMULAR
-                <tr>
-                    <td></td>
-                    <td><input type="submit" value="Odeslat!" name="odeslat"></td>
-                </tr>
-            </table>
-        </form>
-FORMULAR;
-        return $formular;
-    }
+		public function vkladaciFormular(){
+			$formular = '<form method="post">';
+			for ($zapas=0; $zapas < $this -> pocetZapasu ; $zapas++) {
+				$idTeamu = $this -> poleZapasu[$zapas][2];
+				$formular .= <<<HTML
+					<p>{$this -> poleZapasu[$zapas][0]} : {$this -> poleZapasu[$zapas][1]}</p>
+					<p>
+						<input type="text" name="score[$zapas][0]"> : <input type="text" name="score[$zapas][1]">
+						<input type="hidden" name="id[$zapas]" value="$idTeamu">
+					</p>
+HTML;
+			}
+			$formular .= '<p><input type="submit" value="Zkontrolovat!" name="action"><p>';
+			return $formular;
+		}
 
 
-    public function ziskejOverovaciFormular($pole){
-        $formular = <<<FORMULAR
-            <table>
-FORMULAR;
-        for ($zapas=0; $zapas < 4; $zapas++) {
-            if (preg_match('/^[0-9]+$/', $_POST['score'][$zapas][0])){
-                if (preg_match('/^[0-9]+$/', $_POST['score'][$zapas][1])){
-                    $domaci = $this->tym($zapas,0);
-                    $hoste = $this->tym($zapas,1);
-                    $scoreDomaci = $_POST['score'][$zapas][0];
-                    $scoreHoste = $_POST['score'][$zapas][1];
-                    $formular .= <<<FORMULAR
-                        <tr>
-                            <td><p>$domaci</p></td>
-                            <td><p>$hoste</p></td>
-                        </tr>        
-                        <tr>
-                            <td><p>$scoreDomaci</p></td>
-                            <td><p>$scoreHoste</p></td>
-                        </tr>
-FORMULAR;
-                }
-            }
-        }
-        $formular .= <<<FORMULAR
-            </table>
-FORMULAR;
-        return $formular;
-    }
-}
+		public function overovaciFormular($score){
+			$formular = "";
+			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++) { 
+				if (is_numeric($score[$zapas][0]) and is_numeric($score[$zapas][1])){
+					$formular .= <<<HTML
+					<p>{$this -> poleZapasu[$zapas][0]} : {$this -> poleZapasu[$zapas][1]}</p>
+					<p>{$score[$zapas][0]} : {$score[$zapas][1]}</p>
+HTML;
+				}
+			}
+			return $formular;
+		}
+
+
+		public function byloUzZapsano($idZapasu){
+			$nezapsano = True;
+			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++) {
+				if ($idZapasu[$zapas] != $this -> poleZapasu[$zapas][2]) {
+					$nezapsano = False;
+				}
+			}
+			return $nezapsano;
+		}
+		
+
+	}
