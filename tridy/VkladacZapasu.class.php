39655453663a1ca27730dc42d85bba1fae2ca9a3
mrazekthom <mrazekthom@gmail.com>
2014-01-28 16:49:25 +0100

    Přidána logika zapisování do DB + částečné ošetření uživatelských vstupů

--- tridy/VkladacZapasu.class.php
+++ tridy/VkladacZapasu.class.php
@@ -10,6 +10,9 @@
 		public function __construct($kategorie, $pocetZapasu){
 			require('dbWrapper.class.php'); #Může se smazat
         	dbWrapper::pripoj(); #také smazat
+        	define("BODY_VYHRA", 2);
+        	define("BODY_REMIZA", 1);
+        	define("BODY_PROHRA", 0);
         	$zapasyKategorie = "2014_zapasy_" . $kategorie;
         	$tymyKategorie = "2014_tymy_" . $kategorie;
         	$sql = dbWrapper::dotaz(<<<SQL
@@ -60,15 +63,18 @@ SQL
 
 		public function vkladaciFormular(){
 			$formular = '<form method="post">';
-			for ($zapas=0; $zapas < $this -> pocetZapasu ; $zapas++) {
-				$idTeamu = $this -> poleZapasu[$zapas][2];
-				$formular .= <<<HTML
-					<p>{$this -> poleZapasu[$zapas][0]}:{$this -> poleZapasu[$zapas][1]}</p>
-					<p>
-						<input type="text" name="score[$zapas][0]">:<input type="text" name="score[$zapas][1]">
-						<input type="hidden" name="id[$zapas]" value="$idTeamu">
-					</p>
+			for ($zapas=0; $zapas < $this -> pocetZapasu ; $zapas++){
+				if (isset($this -> poleZapasu[$zapas][2])){
+					$idTeamu = $this -> poleZapasu[$zapas][2];
+					$formular .= <<<HTML
+						<p>{$this -> poleZapasu[$zapas][0]}:{$this -> poleZapasu[$zapas][1]}</p>
+						<p>
+							<input type="text" name="score[$zapas][0]">:<input type="text" name="score[$zapas][1]">
+							<input type="hidden" name="id[$zapas]" value="$idTeamu">
+						</p>
 HTML;
+				}
+				
 			}
 			$formular .= '<p><input type="submit" value="Zkontrolovat!" name="action"><p>';
 			$formular .= '</form>';
@@ -78,49 +84,100 @@ HTML;
 
 		public function overovaciFormular($post){
 			$_SESSION['ID'] = $post;
-			$score = $post['score'];
+			print_r($_SESSION);
 			$formular = "";
-			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++) { 
-				if (is_numeric($score[$zapas][0]) && is_numeric($score[$zapas][1])){
-					$formular .= <<<HTML
-					<p>{$this -> poleZapasu[$zapas][0]}:{$this -> poleZapasu[$zapas][1]}</p>
-					<p>{$score[$zapas][0]}:{$score[$zapas][1]}</p>
-HTML;
+			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++){
+				if (isset($this -> poleZapasu[$zapas][2])){
+					$scoreDomaci = trim($post['score'][$zapas][0]);
+					$scoreHoste = trim($post['score'][$zapas][1]);
+					if (is_numeric($scoreDomaci) && is_numeric($scoreHoste)){
+						$formular .= $this -> zvyrazniTymVyhercu($zapas, $scoreDomaci, $scoreHoste);
+						$formular .= "<p>{$scoreDomaci}:{$scoreHoste}</p>";
+					}
 				}
 			}
-			$formular .= "
-			<p>
-				<form method='post'><input type='submit' value='Zapsat!' name='action'></form>
-				<input onclick='javascript:self.history.back(); type=button value='Zpět k zadání výsledků'>
-			<p>";
-			
+			$formular .= <<<HTML
+				<p>
+					<form method='post'><input type='submit' value='Zapsat!' name='action'></form>
+					<input onclick=javascript:self.history.back(); type=button value='Zpět k zadání výsledků'>
+				<p>
+HTML;
 			return $formular;
 		}
 
 
 		public function zapisDB($post){
-			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++) {
-				if (is_numeric($post['score'][$zapas][0]) && is_numeric($post['score'][$zapas][1])){
-					$sql = dbWrapper::dotaz(<<<SQL
-						UPDATE `2014_zapasy_mladsi` 
-						SET `SCR_domaci`=?,`SCR_hoste`=?,`cas_vlozeni`=NOW(),`odehrano`='1'
-						WHERE `ID_zapasu`=?
+			$zapasyKategorie = "2014_zapasy_" . $this -> kategorie;
+        	$tymyKategorie = "2014_tymy_" . $this -> kategorie;
+        	$idTeamu = array("ID_domaci", "ID_hoste");
+			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++){
+				if (isset($this -> poleZapasu[$zapas][2])){
+					$scoreDomaci = trim($post['score'][$zapas][0]);
+					$scoreHoste = trim($post['score'][$zapas][1]);
+					if (is_numeric($scoreDomaci) && is_numeric($scoreHoste)){
+						$bodyTym = $this -> ziskaneBody($scoreDomaci, $scoreHoste);
+						$sql = dbWrapper::dotaz(<<<SQL
+							UPDATE $zapasyKategorie 
+							SET `SCR_domaci`=?,`body_domaci`=?,`SCR_hoste`=?,`body_hoste`=?,`cas_vlozeni`=NOW(),`odehrano`='1'
+							WHERE `ID_zapasu`=?
 SQL
-					, Array($post['score'][$zapas][0], $post['score'][$zapas][1], $post['id'][$zapas]));
+						, Array($scoreDomaci, $bodyTym[0], $scoreHoste, $bodyTym[1], $post['id'][$zapas]));
+						for ($tym=0; $tym < 2; $tym++){ 
+							if ($bodyTym[$tym] != 0){
+								$sql = dbWrapper::dotaz(<<<SQL
+									UPDATE `2014_tymy_mladsi` a
+									INNER JOIN `2014_zapasy_mladsi` b ON
+										a.`id_teamu` = b.{$idTeamu[$tym]}
+									SET `body`=`body`+ ?
+									WHERE b.`ID_zapasu` = ?
+SQL
+								, array($bodyTym[$tym], $post['id'][$zapas]));
+							}
+						}
+					}
 				}
 			}
+		}
+
+
+		public function ziskaneBody($scoreDomaci, $scoreHoste){
+			if ($scoreDomaci > $scoreHoste){
+				$body = array(BODY_VYHRA, BODY_PROHRA);
+			}elseif ($scoreDomaci == $scoreHoste){
+				$body = array(BODY_REMIZA, BODY_REMIZA);
+			}elseif ($scoreDomaci < $scoreHoste){
+				$body = array(BODY_PROHRA, BODY_VYHRA);
+			}
+			return $body;
+		}
+
+
+		public function zvyrazniTymVyhercu($idZapasu, $scoreDomaci, $scoreHoste){
+			if ($scoreDomaci > $scoreHoste){
+				$formular = "<p><b>{$this -> poleZapasu[$idZapasu][0]}</b>:{$this -> poleZapasu[$idZapasu][1]}</p>";
+			}elseif ($scoreDomaci == $scoreHoste){
+				$formular = "<p>{$this -> poleZapasu[$idZapasu][0]}:{$this -> poleZapasu[$idZapasu][1]}</p>";
+			}elseif ($scoreDomaci < $scoreHoste){
+				$formular = "<p>{$this -> poleZapasu[$idZapasu][0]}:<b>{$this -> poleZapasu[$idZapasu][1]}</b></p>";
+			}else{
+				$formular = "";
+			}
+			return $formular;
 		}
 
 
 		public function byloZapsano($idZapasu){
 			$nezapsano = True;
-			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++) {
-				if ($idZapasu[$zapas] != $this -> poleZapasu[$zapas][2]) {
-					$nezapsano = False;
+			for ($zapas=0; $zapas < $this -> pocetZapasu; $zapas++){
+				if (isset($idZapasu[$zapas])){
+					if ($idZapasu[$zapas] != $this -> poleZapasu[$zapas][2]){
+						$nezapsano = False;
+					}
 				}
+				
 			}
 			return $nezapsano;
 		}
-		
+
 
 	}
