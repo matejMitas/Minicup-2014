a1387f1ed698d0213689d58d36221aa88f0b0f00
Joe Kolář <thejoeejoee@gmail.com>
2014-02-10 22:09:55 +0100

    Přidána volba kategorie

--- tridy/VystupVysledku.class.php
+++ tridy/VystupVysledku.class.php
@@ -1,21 +1,31 @@
 <?php
 class VystupVysledku {
+
+    /**
+     * kategorie pro generovani tohoto vystupu
+     * @var string
+     * @access private
+     */
+	private $kategorie;
+
+
     /**
      * ziskejOdehraneZapasy
-     * 
      * @param time $datum datum pro vygenerování zápasů
-     *
      * @access public
-     *
      * @return array[string] vrátí raw data všech zápasů
      */
+    
+    function __construct($kategorie) {
+    	$this->kategorie = $kategorie;
+    }
 	public function ziskejOdehraneZapasy($datum){
 		$sql=<<<SQL
 			SELECT 
 			b.`jmeno`,c.`jmeno`,a.`SCR_domaci`,`SCR_hoste`
-			FROM `2014_zapasy_mladsi` a
-			JOIN `2014_tymy_mladsi` b ON a.`ID_domaci`=b.`ID_teamu`
-			JOIN `2014_tymy_mladsi` c ON a.`ID_hoste`=c.`ID_teamu`
+			FROM `2014_zapasy_{$this->kategorie}` a
+			JOIN `2014_tymy_{$this->kategorie}` b ON a.`ID_domaci`=b.`ID_teamu`
+			JOIN `2014_tymy_{$this->kategorie}` c ON a.`ID_hoste`=c.`ID_teamu`
 			WHERE a.`odehrano`=1 
 			ORDER BY a.`cas_odehrani` ASC 
 SQL;
@@ -30,11 +40,11 @@ SQL;
 		SELECT `poradi`,`jmeno`,ifnull(sum(D),0) dane, ifnull(sum(H),0) dostane, `body`
 			FROM (
 			SELECT sum(`SCR_domaci`) D, sum(`SCR_hoste`) H, `ID_domaci` Team
-			FROM `2014_zapasy_mladsi`
+			FROM `2014_zapasy_{$this->kategorie}`
 			GROUP BY Team
 			UNION
 			SELECT sum(`SCR_hoste`) D, sum(`SCR_domaci`) H, `ID_hoste` Team
-			FROM `2014_zapasy_mladsi`
+			FROM `2014_zapasy_{$this->kategorie}`
 			GROUP BY Team
 		)CLK
 		RIGHT JOIN 2014_tymy_mladsi TM ON CLK.Team = TM.id_teamu
@@ -43,4 +53,4 @@ SQL;
 SQL;
 	return dbWrapper::dotaz($SQL,Array())->FetchAll();
 	}
-	}
+}
