603f2cca99be34a66d2bf6920c1ea341f5985180
Joe Kolář <thejoeejoee@gmail.com>
2014-04-08 22:01:14 +0200

    odložení týmů

--- kontrolery/tymy.php
+++ kontrolery/tymy.php
@@ -2,3 +2,10 @@
 
 use Nette\Templating\FileTemplate;
 $template = new FileTemplate('sablony/tymy.latte');
+
+
+// $VkladacZapasu = new VkladacZapasu('mladsi', 4);
+$DetailTymu = new DetailTymu('mladsi', rand(1,12));
+$DetailTymu->ziskejNazevTymu();
+$DetailTymu->ziskejPoradiSkore()." Úspěšnost: ".$DetailTymu->ziskejProcentualniUspech()." %";
+$DetailTymu->ziskejOdehraneZapasy();
