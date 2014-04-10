cd3871b5a623e1bc245c88d04202272d78faa9e7
Joe Kolář <thejoeejoee@gmail.com>
2014-04-10 22:28:11 +0200

    lepší formulář

--- tridy/Novinkovac.class.php
+++ tridy/Novinkovac.class.php
@@ -16,12 +16,12 @@ SQL
 public function ziskejVkladaciFormular() {
 	return <<<VYSTUP
 	<form action="" method="POST">
-		<label for="titulek">Titulek:</label>
+		<h3>Titulek:</h3>
 		<input type="text" name="titulek" required="required">
 		<br>
 
-		<label for="aktualita">Aktualita:</label>
-		<textarea name="aktualita" id="aktualita" placeholder="Zde vyplňte aktualitu k přidání.." cols="75" rows="8" required="required">
+		<h3>Aktualita:</h3>
+		<textarea style="max-width:97%;" name="aktualita" id="aktualita" placeholder="Zde vyplňte aktualitu k přidání.." cols="75" rows="8" required="required">
 		</textarea>
 
 		<input type="submit" value="Odeslat novinku">
