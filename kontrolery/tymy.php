ef0ea96fd21812bcad68ad4c415c23f8ee66853b
Joe Kolář <thejoeejoee@gmail.com>
2014-04-21 20:20:39 +0200

    hotov detail týmu

--- kontrolery/tymy.php
+++ kontrolery/tymy.php
@@ -1,11 +1,19 @@
 <?php
 
 use Nette\Templating\FileTemplate;
-$template = new FileTemplate('sablony/tymy.latte');
+
+
+if (isset($_GET["cat"],$_GET["id"])) {
+    $template = new FileTemplate('sablony/detail.latte'); 
+    $DetailTymu = new DetailTymu($_GET["cat"], $_GET["id"]);
+    $template->jmeno = $DetailTymu->ziskejNazevTymu();
+    $template->data = $DetailTymu->ziskejPoradiSkore();
+    $DetailTymu->ziskejProcentualniUspech();
+    $template->zapasy = $DetailTymu->ziskejOdehraneZapasy();
+} else {
+    $template = new FileTemplate('sablony/tymy.latte');
+    $template->tymy = Array("mladsi" => DetailTymu::ziskejVsechnyTymy("mladsi"), "starsi" => DetailTymu::ziskejVsechnyTymy("starsi"));
+}
 
 
 // $VkladacZapasu = new VkladacZapasu('mladsi', 4);
-$DetailTymu = new DetailTymu('mladsi', rand(1,12));
-$DetailTymu->ziskejNazevTymu();
-$DetailTymu->ziskejPoradiSkore()." Úspěšnost: ".$DetailTymu->ziskejProcentualniUspech()." %";
-$DetailTymu->ziskejOdehraneZapasy();
