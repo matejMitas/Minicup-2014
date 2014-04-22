4e5d29c826eb835d41d193e8ccb635495b8a536e
Joe Kolář <thejoeejoee@gmail.com>
2014-04-22 22:28:40 +0200

    uklízíme

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-$(function(){
-	
+$(function() {
+
 });
 
 $(function() {
-		var pull		= $('#pull');
-			menu		= $('nav ul');
-			menuHeight  = menu.height();
-		$(pull).on('click', function(e) {
-			e.preventDefault();
-			menu.slideToggle();
-		});
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
 
-		$(window).resize(function(){
-			var w = $(window).width();
-			if(w > 320 && menu.is(':hidden')) {
-				menu.removeAttr('style');
-				}
-		});
-	});
+    $(window).resize(function() {
+        var w = $(window).width();
+        if (w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
