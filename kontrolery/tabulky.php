e6e9204cc0b891bf29b4c02b8bbd847597b5f739
Joe Kolář <thejoeejoee@gmail.com>
2014-04-22 22:27:34 +0200

    Trocha toho uklázení

--- kontrolery/tabulky.php
+++ kontrolery/tabulky.php
@@ -1,16 +1,17 @@
 <?php
 
 use Nette\Templating\FileTemplate;
+
 $template = new FileTemplate('sablony/tabulky.latte');
 
 
-$template->rozlosovani= array(
-								"mladsi" => array(
-												$VystupVysledkuML->ziskejRozlosovaniZapasu("2014-05-23 00:00:00"),
-												$VystupVysledkuML->ziskejRozlosovaniZapasu("2014-05-24 00:00:00"),
-												$VystupVysledkuML->ziskejRozlosovaniZapasu("2014-05-25 00:00:00")),
-								"starsi" => array(
-												$VystupVysledkuST->ziskejRozlosovaniZapasu("2014-05-23 00:00:00"),
-												$VystupVysledkuST->ziskejRozlosovaniZapasu("2014-05-24 00:00:00"),
-												$VystupVysledkuST->ziskejRozlosovaniZapasu("2014-05-25 00:00:00"))
-							);
+$template->rozlosovani = array(
+    "mladsi" => array(
+        $VystupVysledkuML->ziskejRozlosovaniZapasu("2014-05-23 00:00:00"),
+        $VystupVysledkuML->ziskejRozlosovaniZapasu("2014-05-24 00:00:00"),
+        $VystupVysledkuML->ziskejRozlosovaniZapasu("2014-05-25 00:00:00")),
+    "starsi" => array(
+        $VystupVysledkuST->ziskejRozlosovaniZapasu("2014-05-23 00:00:00"),
+        $VystupVysledkuST->ziskejRozlosovaniZapasu("2014-05-24 00:00:00"),
+        $VystupVysledkuST->ziskejRozlosovaniZapasu("2014-05-25 00:00:00"))
+);
