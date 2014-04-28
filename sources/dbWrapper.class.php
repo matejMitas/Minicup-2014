5a4c92c148ba23aa1c6f0061e4b1e59866d7643f
Joe Kolář <thejoeejoee@gmail.com>
2014-04-28 22:26:14 +0200

    změna složky

---
+++ sources/dbWrapper.class.php
@@ -0,0 +1,22 @@
+<?php
+    class dbWrapper {
+
+    private static $spojeni;
+
+    private static $nastaveni = Array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'");
+
+    public static function pripoj() {
+    	include_once './settings.php';
+        self::$spojeni = new PDO("mysql:host=$host;dbname=$databaze",$uzivatel,$heslo,self::$nastaveni);
+    }
+
+    public static function dotaz($sql_string, $parametry = Array()) {
+        $dotaz = self::$spojeni->prepare($sql_string);
+        $dotaz->execute($parametry);
+        return $dotaz;
+    }
+
+}
+?>
